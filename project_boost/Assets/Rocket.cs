08ce7406f91c12f4ec705c9596183d22e0160f6d
kiserr10 <kiserr10@gmail.com>
2018-04-15 18:52:50 -0600

    level 2 layout complete

--- project_boost/Assets/Rocket.cs
+++ project_boost/Assets/Rocket.cs
@@ -6,6 +6,7 @@ public class Rocket : MonoBehaviour {
 
 	[SerializeField] float rcsThrust = 100f;
 	[SerializeField] float mainThrust = 1000f;
+	
 
 	Rigidbody rigidBody;
 	AudioSource audioSource;
@@ -17,36 +18,44 @@ public class Rocket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		float thrustThisFrame = mainThrust * Time.deltaTime;
 		Rotate();
-		Thrust();
+		Thrust(thrustThisFrame);
+		Reverse(thrustThisFrame);
 	}	
 
 	void OnCollisionEnter(Collision collision){
 		switch (collision.gameObject.tag){
 			case "Friendly":
-				print("friendly");
+				print("Safe");
 				break; 
 			case "Fuel":
-				print("fuel bitches");
+				print("Fuel Aquired!");
+				break;
+			case "Gold":
+				print("Gold Aquired!");
 				break;
 			default:
-				print("You're DEAD");
+				print("TOAST!");
 				break;
 		}
 	}
 
-	private void Thrust () {
-
+	private void Thrust (float thrust) {
 		float thrustThisFrame = mainThrust * Time.deltaTime;
-
-		if (Input.GetKey(KeyCode.Space)) {
-			rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
+			if (Input.GetKey(KeyCode.Space)) {
+			rigidBody.AddRelativeForce(Vector3.up * thrust);
 			if (!audioSource.isPlaying){
 				audioSource.Play();
 			}
 		}
 		if (Input.GetKeyUp(KeyCode.Space)) {
 			audioSource.Stop();
+		}
+	}
+	private void Reverse (float thrust) {
+		if(Input.GetKey(KeyCode.W)) {
+			rigidBody.AddRelativeForce(Vector3.down * thrust);
 		}
 	}
 
