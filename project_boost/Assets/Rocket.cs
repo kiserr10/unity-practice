17d1e260e4f5d0f2594b7e43305cf3f5a8c90974
kiserr10 <kiserr10@gmail.com>
2018-04-15 20:38:51 -0600

    level redirect in place for boost

--- project_boost/Assets/Rocket.cs
+++ project_boost/Assets/Rocket.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour {
 
 	[SerializeField] float rcsThrust = 100f;
 	[SerializeField] float mainThrust = 1000f;
 	
+	enum State { Alive, Dying, Transcending };
 
 	Rigidbody rigidBody;
 	AudioSource audioSource;
+	State state;
+	
+
 	// Use this for initialization
+
 	void Start () {
 		rigidBody = GetComponent<Rigidbody>();
 		audioSource = GetComponent<AudioSource>();
@@ -19,31 +25,55 @@ public class Rocket : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float thrustThisFrame = mainThrust * Time.deltaTime;
-		Rotate();
-		Thrust(thrustThisFrame);
-		Reverse(thrustThisFrame);
+		if(isAlive()) {
+			Rotate();
+			Thrust(thrustThisFrame);
+			Reverse(thrustThisFrame);
+		}
 	}	
 
 	void OnCollisionEnter(Collision collision){
+		
+		if(!isAlive()){ return; }
+
 		switch (collision.gameObject.tag){
 			case "Friendly":
-				print("Safe");
 				break; 
-			case "Fuel":
-				print("Fuel Aquired!");
-				break;
 			case "Gold":
 				print("Gold Aquired!");
 				break;
+			case "Finish":
+				state = State.Transcending;
+				Invoke("AdvanceNextLevel", 1f);
+				break;
+			case "Win":
+				Invoke("Win", 1f);
+				break;
 			default:
-				print("TOAST!");
+				state = State.Dying;
+				audioSource.Stop();
+				Invoke("LoadFirstLevel", 1f);
 				break;
 		}
+	}
+	private void AdvanceNextLevel () {
+		SceneManager.LoadScene(1);
+	}
+	private void LoadFirstLevel () {
+		SceneManager.LoadScene(0);
+	}
+	private void Win () {
+		state = State.Alive;
+		print("Game Won!");
+		SceneManager.LoadScene(0);
+	}
+	private bool isAlive () {
+		return (state == State.Alive);
 	}
 
 	private void Thrust (float thrust) {
 		float thrustThisFrame = mainThrust * Time.deltaTime;
-			if (Input.GetKey(KeyCode.Space)) {
+		if (Input.GetKey(KeyCode.Space) && isAlive()) {
 			rigidBody.AddRelativeForce(Vector3.up * thrust);
 			if (!audioSource.isPlaying){
 				audioSource.Play();
